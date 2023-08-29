4ab23a9f7acd155ec179cf3db9359b0dfd7b1ebf
ALTGAME <krgpsh13@gmail.com>
2023-08-29 22:07:28 +0530

    expense

--- routes/transcations.js
+++ routes/transcations.js
@@ -1,12 +1,16 @@
  const path = require('path');
  const transactionController = require('../controllers/income');
-
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
 
 const router = require('express').Router()
 
 
-
 router.post('/add-income', transactionController.addIncome)
 router.get('/get-incomes',transactionController.getIncomes)
-router.delete('/delete-income/:id', transactionController.deleteIncome)
+router.delete('/delete-income', transactionController.deleteIncome)
+
+router.post('/add-expense', addExpense)
+router.get('/get-expenses', getExpense)
+router.delete('/delete-expenses/:id',deleteExpense)
+
 module.exports = router
